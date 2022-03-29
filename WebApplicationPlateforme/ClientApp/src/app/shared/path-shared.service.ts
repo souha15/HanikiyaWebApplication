9f37a3711e50075b7677621f9361f2c13c7df4e5
Khemiri souha <khemirisouhaa@gmail.com>
2022-03-29 10:09:48 +0100

    First Push

--- WebApplicationPlateforme/ClientApp/src/app/shared/path-shared.service.ts
+++ WebApplicationPlateforme/ClientApp/src/app/shared/path-shared.service.ts
@@ -9,8 +9,8 @@ export class PathSharedService {
 
   getPath() {
 //return  'http://161.97.163.78:82/api'
-//return  'http://161.97.163.78:88/api'
-  return 'http://localhost:49599/api'
+return  'http://161.97.163.78:88/api'
+//  return 'http://localhost:49599/api'
  // return 'http://161.97.163.78:84/api'
  //  return 'http://173.212.215.254:80/api'
 
